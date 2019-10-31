290900c3681b38dde4385ff63668f8cc354d9fa6
JamestheDon <jamesdonhawkins@gmail.com>
2019-10-31 12:56:28 -0700

    added neon sign effect

---
+++ client/src/Components/effects/NeonSign.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import styles from "./NeonSign.module.css";
+
+function NeonSign() {
+  return (
+    <div className={` container ${styles.span}`}>
+      <div className={` ${styles.lines}`}>
+        <span className={`${styles.span} ${styles.line}`}>
+          I want it to
+          <span
+            className={`${styles.span} ${styles.well}`}
+            style={{ "--i": 0 }}
+          >
+            {" "}
+            work
+          </span>
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default NeonSign;
--- client/src/Components/main/Landing.js
+++ client/src/Components/main/Landing.js
@@ -1,14 +1,14 @@
 import React from "react";
 
 import styles from "./Landing.module.css";
-import james from "../../img/jamesiii_drake.ico";
+import NeonSign from "../effects/NeonSign";
 import TypeWriter from "../effects/TypeWriter";
-import { Link } from "react-router-dom";
 
 function Landing() {
   return (
     <div className={`container ${styles.screen1}`}>
       <div className="row justify-content-center">
+        <NeonSign />
         <div className="col-md-8">
           <div className={`jumbotron text-left  ${styles.jumbotron}`}>
             <TypeWriter
